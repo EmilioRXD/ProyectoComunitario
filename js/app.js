9884fae433555905179eb11c9f3bf8f4c8245713
EmilioRXD <jes99.444.rodriguez@gmail.com>
2024-08-07 01:43:34 -0400

    Integracion animacion con css y js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function desvanecer() {
     const elemento = document.querySelector('.miembro1');
-    elemento.classList.add('oculto');
+    elemento.classList.add('mostrar');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const selectElementBoss = document.getElementById('boss');
+
+    selectElementBoss.addEventListener('change', async function() {
+        const selectedOption = selectElementBoss.options[selectElementBoss.selectedIndex].value;
+        if (selectedOption == 1) {
+            document.querySelector('.carga').classList.remove('mostrar');
+            await sleep(1000)
+            document.querySelector('.familia').classList.add('mostrar');
+        } else {
+            document.querySelector('.familia').classList.remove('mostrar');
+            await sleep(1000)
+            document.querySelector('.carga').classList.add('mostrar');
+        }
+    });
+});
