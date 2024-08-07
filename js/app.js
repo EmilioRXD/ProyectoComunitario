b1c89d793252d19c88a407c08c4ca308957a64ab
EmilioRXD <jes99.444.rodriguez@gmail.com>
2024-08-07 16:55:08 -0400

    Incorporacion de formularios y nuevas funcion js para desvanecer los formularios

--- js/app.js
+++ js/app.js
@@ -2,15 +2,10 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function desvanecer() {
-    const elemento = document.querySelector('.miembro1');
-    elemento.classList.add('mostrar');
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const selectElementBoss = document.getElementById('boss');
 
-    selectElementBoss.addEventListener('change', async function() {
+    selectElementBoss.addEventListener('change', async function () {
         const selectedOption = selectElementBoss.options[selectElementBoss.selectedIndex].value;
         if (selectedOption == 1) {
             document.querySelector('.carga').classList.remove('mostrar');
@@ -20,6 +15,31 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector('.familia').classList.remove('mostrar');
             await sleep(1000)
             document.querySelector('.carga').classList.add('mostrar');
+        }
+    });
+
+    const inputElementFamily = document.getElementById('family');
+
+    inputElementFamily.addEventListener('input', function () {
+        const selectedOption = Number(inputElementFamily.value);
+
+        // Agragar Validación de datos negativos y mayores a 8 y menores a 0
+
+        const members = ['.member1', '.member2', '.member3', '.member4', '.member5', '.member6', '.member7', '.member8'];
+
+        if (selectedOption == 0) {
+            let elements = document.querySelectorAll('.formmember');
+            elements.forEach(element => {
+                element.classList.remove('mostrar');
+            });
+        } else {
+            members.forEach((member, index) => {
+                let elements = document.querySelector(member)
+                if (index < selectedOption) {
+                    elements.classList.add('mostrar')
+                }
+            });
+
         }
     });
 });
