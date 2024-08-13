1d80b2f3356c3705588c30b59f5ac74b7dc6af5b
EmilioRXD <jes99.444.rodriguez@gmail.com>
2024-08-13 18:09:47 -0400

    Terminado el formulario, y la conexión con la base de datos

--- js/connection.js
+++ js/connection.js
@@ -1,9 +1,12 @@
+const url = 'https://bptstnuslddxmbsbenpm.supabase.co/rest/v1/';
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdHN0bnVzbGRkeG1ic2JlbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5NTQ2MjQsImV4cCI6MjAzNzUzMDYyNH0.efhMi9nfxrcgRhg4LAF4pCCrZ8FH5DzML-xIf7Ku1-M';
+
 function getDataBaseSupabase() {
-  fetch('https://bptstnuslddxmbsbenpm.supabase.co/rest/v1/countries?select=*', {
+  fetch(url, {
     method: 'GET',
     headers: {
-      'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdHN0bnVzbGRkeG1ic2JlbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5NTQ2MjQsImV4cCI6MjAzNzUzMDYyNH0.efhMi9nfxrcgRhg4LAF4pCCrZ8FH5DzML-xIf7Ku1-M', // Reemplaza con tu clave API
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdHN0bnVzbGRkeG1ic2JlbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5NTQ2MjQsImV4cCI6MjAzNzUzMDYyNH0.efhMi9nfxrcgRhg4LAF4pCCrZ8FH5DzML-xIf7Ku1-M' // Reemplaza con tu clave API
+      'apikey': apiKey,
+      'Authorization': `Bearer ${apiKey}`
     }
   })
     .then(response => {
@@ -16,36 +19,91 @@ function getDataBaseSupabase() {
     .catch(error => console.error('There has been a problem with your fetch operation:', error));
 }
 
+
 const form = document.getElementById('system-form');
 
 function postForm() {
-  const url = 'https://bptstnuslddxmbsbenpm.supabase.co/rest/v1/Persona';
-  const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdHN0bnVzbGRkeG1ic2JlbnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5NTQ2MjQsImV4cCI6MjAzNzUzMDYyNH0.efhMi9nfxrcgRhg4LAF4pCCrZ8FH5DzML-xIf7Ku1-M';
+  const person = [];
+  const relationship = [];
 
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'apikey': apiKey,
-      'Authorization': `Bearer ${apiKey}`,
-      'Content-Type': 'application/json',
-      'Prefer': 'return=minimal'
-    },
-    body: JSON.stringify({
-      cedula: form.cedula.value,
-      firstname: form.firstname.value,
-      lastname: form.lastname.value,
-      gender: form.gender.value,
-      kinship: form.boss.value==1 ? 'Jefe': form.kinship.value,
-      boss: form.streetBoss.value
-    })
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log('Data successfully sent.');
-      } else {
-        console.error('Error:', response.statusText);
-      }
-    })
-    .catch(error => console.error('Request failed:', error));
+  if (form.boss.value == 1) {
+    for (let i = 1; i <= form.family.value; i++) {
+      person.push({
+        cedula: document.getElementById(`cedula${i}`).value,
+        firstname: document.getElementById(`firstname${i}`).value,
+        lastname: document.getElementById(`lastname${i}`).value,
+        gender: document.getElementById(`gender${i}`).value,
+        kinship: i == 1 ? 'Jefe de Familia' : document.getElementById(`kinship${i}`).value,
+        boss: form.streetBoss.value
+      });
+    }
+    for (let i = 2; i <= form.family.value; i++) {
+      relationship.push({
+        cedula_jefe: form.cedula1.value,
+        cedula: document.getElementById(`cedula${i}`).value
+      })
+    }
 
+    fetch(url + 'Persona', {
+      method: 'POST',
+      headers: {
+        'apikey': apiKey,
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+        'Prefer': 'return=minimal'
+      },
+      body: JSON.stringify(person)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Data successfully sent.');
+        } else {
+          console.error('Error:', response.statusText);
+        }
+      })
+      .catch(error => console.error('Request failed:', error));
+
+    fetch(url + 'Relacion', {
+      method: 'POST',
+      headers: {
+        'apikey': apiKey,
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+        'Prefer': 'return=minimal'
+      },
+      body: JSON.stringify(relationship)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Data successfully sent.');
+        } else {
+          console.error('Error:', response.statusText);
+        }
+      })
+      .catch(error => console.error('Request failed:', error));
+
+    fetch(url + 'Contacto', {
+      method: 'POST',
+      headers: {
+        'apikey': apiKey,
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+        'Prefer': 'return=minimal'
+      },
+      body: JSON.stringify({
+        cedula: form.cedula1.value,
+        phone: form.phone.value,
+        email: form.email.value,
+        address: form.address.value
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Data successfully sent.');
+        } else {
+          console.error('Error:', response.statusText);
+        }
+      })
+      .catch(error => console.error('Request failed:', error));
+  }
 }
